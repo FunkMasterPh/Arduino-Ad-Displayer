9e6f3cd076615260cc39ebe65460b983b4371b83
fistbumpfromdestiny <fredrik@wizardry.se>
2021-11-13 08:07:51 +0100

    Added loop to get total sum

--- time_distribution.hpp
+++ time_distribution.hpp
@@ -1,12 +1,17 @@
 #ifndef TIME_DISTRIBUTION_H
 #define TIME_DISTRIBUTION_H
 #include "ad_class.hpp"
+#define TIME 60
 
 int timeDist(vector<Ad>& newAdList){
     int totalSum;
-    int cps;
-    totalSum = obj1.getPaid() + obj2.getPaid();
-    cps = totalSum / 60;
+    int cps; // cost per second
+    int size = newAdList.size();
+   
+    for(int i = 0; i < size; i++)
+        totalSum += newAdList[i].getPaid();
+
+    cps = totalSum / TIME;
     
     return totalSum;
 }
