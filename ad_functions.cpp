941c8bb07fbdcda56c855ef3d3002f67af3e2611
Philip Holmgren <philip.holmgren@live.com>
2021-11-15 10:38:21 +0100

    moved vector functions to h and c files

--- ad_functions.cpp
+++ ad_functions.cpp
@@ -1,8 +1,8 @@
 #include <vector>
 #include "ad_class.hpp"
+#include "ad_functions.hpp"
 
 void fillVector(vector<Ad>& newAdList){
-
     string name;
     string msg;
     int paid;
@@ -20,14 +20,16 @@ void fillVector(vector<Ad>& newAdList){
     Ad newAd(name, msg, paid);
     newAdList.push_back(newAd);
 
-
-
 }
+
 void printVector(vector<Ad>& newAdList){
     int size = newAdList.size();
 
+    for(vector<Ad>::iterator it = newAdList.begin(); it != newAdList.end(); it++)
     for(int i = 0; i < size; i++){
         cout << "Company Name: " << newAdList[i].getName() << endl;
-        cout << "Company Message" << newAdList[i].getMsg() << endl;
+        cout << "Company Message: " << newAdList[i].getMsg() << endl;
+        cout << "Company Offer: " << newAdList[i].getPaid() << endl;
+        cout << "Company Ad Time: " << newAdList[i].getAdTime() << endl;
     }
 }
--- main.cpp
+++ main.cpp
@@ -9,44 +9,11 @@
 #include <fstream>
 #include "ad_class.hpp"
 #include "time_distribution.hpp"
+#include "ad_functions.hpp"
 
 using namespace std;
 
 //int serial_port = open("/dev/cu.usbmodem1442201", O_RDWR);
-
-void fillVector(vector<Ad>&);
-void printVector(vector<Ad>&);
-
-void fillVector(vector<Ad>& newAdList){
-    string name;
-    string msg;
-    int paid;
-
-    cout << "Enter company name: " << endl;
-    cin >> name;
-    while((getchar()) != '\n');
-    cout << "Enter message: " << endl;
-    cin >> msg;
-    while((getchar()) != '\n');
-    cout << "Add money: " << endl;
-    cin >> paid;
-    while((getchar()) != '\n');
-
-    Ad newAd(name, msg, paid);
-    newAdList.push_back(newAd);
-
-}
-
-void printVector(vector<Ad>& newAdList){
-    int size = newAdList.size();
-
-    for(int i = 0; i < size; i++){
-        cout << "Company Name: " << newAdList[i].getName() << endl;
-        cout << "Company Message: " << newAdList[i].getMsg() << endl;
-        cout << "Company Offer: " << newAdList[i].getPaid() << endl;
-        cout << "Company Ad Time: " << newAdList[i].getAdTime() << endl;
-    }
-}
 
 int main(int argc, char **argv){
     //int serial_port;
